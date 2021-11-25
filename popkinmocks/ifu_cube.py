eaf66d5be091db298911f4c0c2d3cf549b9e741b
prashjet <prashinjethwa@gmail.com>
2021-11-25 13:28:51 +0100

    Add method to save output with numpy

--- popkinmocks/ifu_cube.py
+++ popkinmocks/ifu_cube.py
@@ -91,6 +91,31 @@ class IFUCube(object):
         with open(direc + fname, 'wb') as f:
             dill.dump(self, f)
 
+    def save_numpy(self,
+                   v_edg=None,
+                   direc=None,
+                   fname=None):
+        if os.path.isdir(direc) is False:
+            os.mkdir(direc)
+        if v_edg is None:
+            v_edg = np.arange(-1000, 1001, self.ssps.dv)
+        u_edg = np.log(1. + v_edg/self.ssps.speed_of_light)
+        p_tvxz = self.get_p('tvxz', v_edg, density=True, collapse_cmps=True)
+        f_xvtz = np.moveaxis(p_tvxz, [0,1,2,3,4], [3,2,0,1,4])
+        np.savez(direc + fname,
+                 nx1=self.nx,
+                 nx2=self.ny,
+                 x1rng=self.xrng,
+                 x2rng=self.yrng,
+                 S=self.ssps.Xw.reshape((-1,)+self.ssps.par_dims),
+                 w=self.ssps.w,
+                 z_bin_edges=self.ssps.par_edges[0],
+                 t_bin_edges=self.ssps.par_edges[1],
+                 ybar=self.ybar,
+                 y=self.yobs,
+                 v_edg=v_edg,
+                 f_xvtz=f_xvtz)
+
     def get_p(self,
               which_dist,
               collapse_cmps=False,
