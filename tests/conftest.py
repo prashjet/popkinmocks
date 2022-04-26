ca16563b60b5cc593133cecb36a9154a8183ee23
Prashin Jethwa <prashinjethwa@gmail.com>
2022-04-26 16:53:25 +0200

    Reparam disk (#11)
    * Reparameterise disk models
    
    - all disk maps now parmeterised by `q` rather than `sig_x` and `sig_y`
    - change functional form of disk rotation curve to have a core radius, but fixed alpha=3
    - use `cos theta` to raise rotation curve to a 2D `mu(v)` map
    - fix divide by 0 warning
    
    * Update tests for new disk parameterisation
    
    * Update notebook

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ import popkinmocks as pkm
 # growing disks
 ################################
 
-@pytest.fixture(scope="module")
 def my_component():
     ssps = pkm.model_grids.milesSSPs()
     ssps.logarithmically_resample(dv=100.)
@@ -16,60 +15,60 @@ def my_component():
     cube = pkm.ifu_cube.IFUCube(ssps=ssps, nx=9, ny=10)
     gc1 = pkm.components.growingDisk(cube=cube, rotation=0., center=(0,0))
     gc1.set_p_t(lmd=2., phi=0.8)
-    gc1.set_p_x_t(sig_x_lims=(0.5, 0.2),
-                  sig_y_lims=(0.03, 0.1),
+    gc1.set_p_x_t(q_lims=(0.05, 0.5),
+                  rc_lims=(1., 1.),
                   alpha_lims=(1.2, 0.8))
-    gc1.set_t_dep(sig_x=0.5,
-                  sig_y=0.1,
+    gc1.set_t_dep(q=0.2,
                   alpha=3.,
                   t_dep_in=0.5,
                   t_dep_out=5.)
     gc1.set_p_z_tx()
-    gc1.set_mu_v(sig_x_lims=(0.5, 0.1),
-                 sig_y_lims=(0.1, 0.1),
+    gc1.set_mu_v(q_lims=(0.5, 0.1),
                  rmax_lims=(0.5, 1.1),
-                 vmax_lims=(250., 100.),
-                 vinf_lims=(60., 40.))
-    gc1.set_sig_v(sig_x_lims=(0.7, 0.1),
-                  sig_y_lims=(0.2, 0.1),
+                 vmax_lims=(250., 100.))
+    gc1.set_sig_v(q_lims=(0.25, 1.0),
                   alpha_lims=(3.0, 2.5),
                   sig_v_in_lims=(70., 50.),
                   sig_v_out_lims=(80., 60.))
     gc1.evaluate_ybar()
     return ssps, cube, gc1
 
+@pytest.fixture(scope="module", name='my_component')
+def my_component_fixture():
+    return my_component()
 
-
-@pytest.fixture(scope="module")
-def my_second_component(my_component):
-    ssps, cube, gc1 = my_component
+def my_second_component():
+    ssps = pkm.model_grids.milesSSPs()
+    ssps.logarithmically_resample(dv=100.)
+    ssps.calculate_fourier_transform()
+    ssps.get_light_weights()
+    cube = pkm.ifu_cube.IFUCube(ssps=ssps, nx=9, ny=10)
     gc2 = pkm.components.growingDisk(cube=cube,
                                      rotation=10.,
                                      center=(0.05,-0.07))
     gc2.set_p_t(lmd=1.6, phi=0.3)
-    gc2.set_p_x_t(sig_x_lims=(0.9, 0.5),
-                  sig_y_lims=(0.9, 0.5),
+    gc2.set_p_x_t(q_lims=(1., 1.),
+                  rc_lims=(1., 1.),
                   alpha_lims=(1.1, 1.1))
-    gc2.set_t_dep(sig_x=0.9,
-                  sig_y=0.8,
+    gc2.set_t_dep(q=0.7,
                   alpha=1.1,
                   t_dep_in=6.,
                   t_dep_out=1.)
     gc2.set_p_z_tx()
-    gc2.set_mu_v(sig_x_lims=(0.5, 0.4),
-                 sig_y_lims=(0.4, 0.6),
+    gc2.set_mu_v(q_lims=(0.5, 0.4),
                  rmax_lims=(0.7, 0.2),
-                 vmax_lims=(-150., -190.),
-                 vinf_lims=(-70., -30.))
-    gc2.set_sig_v(sig_x_lims=(0.4, 0.3),
-                  sig_y_lims=(0.3, 0.4),
+                 vmax_lims=(-150., -190.))
+    gc2.set_sig_v(q_lims=(0.3/0.4, 0.4/0.3),
                   alpha_lims=(2.0, 1.5),
                   sig_v_in_lims=(70., 90.),
                   sig_v_out_lims=(10., 20.))
     gc2.evaluate_ybar()
     return gc2
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", name='my_second_component')
+def my_second_component_fixture():
+    return my_second_component()
+
 def my_stream_component():
     ssps = pkm.model_grids.milesSSPs()
     ssps.logarithmically_resample(dv=100.)
@@ -87,13 +86,27 @@ def my_stream_component():
     stream.evaluate_ybar()
     return ssps, cube, stream
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", name='my_stream_component')
+def my_stream_component_fixture():
+    return my_stream_component()
+
 def my_three_component_cube(
     my_component,
     my_second_component,
     my_stream_component):
     """Makes a cube with three components
     """
+    ssps, cube, gc1 = my_component()
+    gc2 = my_second_component()
+    ssps, cube, stream = my_stream_component()
+    cube.combine_components([gc1, gc2, stream], [0.65, 0.25, 0.1])
+    return cube
+
+@pytest.fixture(scope="module", name="my_three_component_cube")
+def my_three_component_cube_fixture(
+    my_component,
+    my_second_component,
+    my_stream_component):
     ssps, cube, gc1 = my_component
     gc2 = my_second_component
     ssps, cube, stream = my_stream_component
